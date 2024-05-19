2a72df64ddfef88ba360a4f5d1112cba3eccab41
Christopher Stewart <chris.stewart132@gmail.com>
2024-05-20 10:37:31 +1200

    end to end test: equipment filter

--- testing/end_to_end_test.mjs
+++ testing/end_to_end_test.mjs
@@ -1,7 +1,13 @@
 /*  
+  DOCS (headless browser)
   https://pptr.dev/
   https://pptr.dev/api/puppeteer.page.title
 
+  HOSTING   
+  Navigate into /docs/ (cd docs)
+  python -m http.server
+
+  TESTING
   run: node --test
 */
 import { test } from "node:test";
@@ -12,6 +18,7 @@ import puppeteer from 'puppeteer';
 const URL = "http://127.0.0.1:8000/index.html"
 
 function sleep(ms) {
+  console.log(`sleeping: ${ms}ms`)
   return new Promise(resolve => {
       setTimeout(resolve, ms);
   });
@@ -30,15 +37,55 @@ async function get_title(page){
 }
 
 async function exercise_count(page){
-  return await page.$eval(`.container`, e => e.childElementCount)
+  let count = await page.evaluate(() => {
+      const container = document.querySelector('.container');
+      let exercises = Array.from(container.children);
+      return exercises.reduce((total, element) => {
+          if (!element.classList.contains('hidden')) {
+              return total + 1;
+          }
+          return total;
+      }, 0);
+  });
+  return count;
 }
 
-test("home page test", { only: true }, async () => {
+
+
+test("home page", { only: true }, async () => {
     let browser = await get_browser()
     let page = await get_home_page(browser)
 
     assert.strictEqual("GymV2", await get_title(page));
-    assert.strictEqual(526, await exercise_count(page));
-
+    assert.strictEqual(525, await exercise_count(page));
+    await page.screenshot({path: `testing/images/home.png`});
     await browser.close();
+});
+
+test("equipment filter", { only: true }, async () => {
+  let browser = await get_browser()
+  let page = await get_home_page(browser)
+  let count
+
+  count = await page.evaluate(() => {
+    const inputElement = document.querySelector('#equipment').value = `${"t-bar"}`;
+    applyFilter()
+    let exercises = Array.from(document.querySelector(`.container`).children)
+    return exercises.reduce((t, e) => {return e.classList.contains(`hidden`) ? t : t+1}, 0)
+  })
+  sleep(1000)
+  await page.screenshot({path: `testing/images/${"t-bar"}.png`});
+  assert.strictEqual(1, count);
+
+  count = await page.evaluate(() => {
+    const inputElement = document.querySelector('#equipment').value = `${"kettlebell"}`;
+    applyFilter()
+    let exercises = Array.from(document.querySelector(`.container`).children)
+    return exercises.reduce((t, e) => {return e.classList.contains(`hidden`) ? t : t+1}, 0)
+  })
+  sleep(1000)
+  await page.screenshot({path: `testing/images/${"kettlebell"}.png`});
+  assert.strictEqual(4, count);
+
+  await browser.close();
 });
