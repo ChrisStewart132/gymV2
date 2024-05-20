12eced2586cf5bbadca067690defc8301a2cc1ec
Christopher Stewart <chris.stewart132@gmail.com>
2024-05-21 09:41:16 +1200

    added light and dark mode test, corrected await sleep

--- testing/end_to_end_test.mjs
+++ testing/end_to_end_test.mjs
@@ -57,6 +57,21 @@ const filter = (select_id, option) => {
   return exercises.reduce((t, e) => {return e.classList.contains(`hidden`) ? t : t+1}, 0)
 }
 
+const all_exercises_in_light_mode = () => {
+  const exercises = Array.from(document.querySelectorAll(`.exercise`))
+  const not_light_mode_count = exercises.reduce((t, e) => {
+    const name = e.querySelector(`#name`)
+    const img = e.querySelector(`img`)
+    const is_light_mode = name.classList.contains(`lightMode`) && !img.src.includes(`exercises_inverted`)
+    return is_light_mode ? t : t+1
+  }, 0)
+  return not_light_mode_count == 0
+}
+
+const toggle_dark_mode = () => {
+  document.querySelector(`#${"darkModeBtn"}`).click()
+}
+
 async function main(){
   let browser = await get_browser()
 
@@ -68,6 +83,25 @@ async function main(){
       await page.screenshot({path: `testing/images/home.png`});
   });
 
+  test("light/dark mode", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    await sleep(SLEEP_MS)
+    // default dark_mode
+    assert.strictEqual(false, await page.evaluate(all_exercises_in_light_mode));
+
+    // turn on light_mode
+    await page.evaluate(toggle_dark_mode)
+    await sleep(SLEEP_MS)
+    assert.strictEqual(true, await page.evaluate(all_exercises_in_light_mode));
+    await page.screenshot({path: `testing/images/light_mode.png`});
+
+    // turn on dark_mode
+    await page.evaluate(toggle_dark_mode)
+    await sleep(SLEEP_MS)
+    assert.strictEqual(false, await page.evaluate(all_exercises_in_light_mode));
+    await page.screenshot({path: `testing/images/dark_mode.png`});
+});
+
   await test("equipment", { only: true }, async () => {
     let page = await get_home_page(browser)
     let count
@@ -75,13 +109,13 @@ async function main(){
     const id = "equipment"
     let option = "t-bar"
     count = await page.evaluate(filter, id, option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     await page.screenshot({path: `testing/images/${option}.png`});
     assert.strictEqual(1, count);
 
     option = "kettlebell"
     count = await page.evaluate(filter, id, option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     await page.screenshot({path: `testing/images/${"kettlebell"}.png`});
     assert.strictEqual(4, count);
   });
@@ -93,7 +127,7 @@ async function main(){
     const id = "searchInput"
     let option = "push"
     count = await page.evaluate(filter, id, option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     await page.screenshot({path: `testing/images/${option}.png`});
     assert.strictEqual(27, count);
   });
@@ -106,7 +140,7 @@ async function main(){
     let option = "Biceps"
     
     count = await page.evaluate(filter, id, option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     await page.screenshot({path: `testing/images/${option}.png`});
     assert.strictEqual(15, count);
   });
@@ -132,7 +166,7 @@ async function main(){
     let option = " Pull"// blame db for the space
     
     count = await page.evaluate(filter, id, option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     await page.screenshot({path: `testing/images/_${option}.png`});
     assert.strictEqual(276, count);
 
@@ -150,31 +184,31 @@ async function main(){
     const force_id = "ForceSelection"
     let force_option = " Pull"// blame db for the space
     count = await page.evaluate(filter, force_id, force_option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     assert.strictEqual(276, count);
 
     const synergist_id = "SynergistMuscleFilter"
     let synergist_option = "Deltoid"
     count = await page.evaluate(filter, synergist_id, synergist_option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     assert.strictEqual(82, count);
 
     const target_id = "TargetMuscleFilter"
     let target_option = "Lateral"
     count = await page.evaluate(filter, target_id, target_option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     assert.strictEqual(14, count);
 
     const search_id = "searchInput"
     let search_option = ""
     count = await page.evaluate(filter, search_id, search_option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     assert.strictEqual(14, count);
 
     const equipment_id = "equipment"
     let equipment_option = "dumbbells"
     count = await page.evaluate(filter, equipment_id, equipment_option)
-    sleep(SLEEP_MS)
+    await sleep(SLEEP_MS)
     assert.strictEqual(8, count);
 
     await page.screenshot({path: `testing/images/${"combined_filter_test"}.png`});
