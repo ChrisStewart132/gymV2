ea1b2bc5894203177f2a034fa1f8cf64483b2f98
Christopher Stewart <chris.stewart132@gmail.com>
2024-05-21 08:55:00 +1200

    refactor test so browser launched once

--- testing/end_to_end_test.mjs
+++ testing/end_to_end_test.mjs
@@ -57,141 +57,129 @@ const filter = (select_id, option) => {
   return exercises.reduce((t, e) => {return e.classList.contains(`hidden`) ? t : t+1}, 0)
 }
 
-test("home page", { only: true }, async () => {
-    let browser = await get_browser()
+async function main(){
+  let browser = await get_browser()
+
+  test("home page", { only: true }, async () => {
+      let page = await get_home_page(browser)
+
+      assert.strictEqual("GymV2", await get_title(page));
+      assert.strictEqual(525, await exercise_count(page));
+      await page.screenshot({path: `testing/images/home.png`});
+  });
+
+  await test("equipment", { only: true }, async () => {
     let page = await get_home_page(browser)
+    let count
 
-    assert.strictEqual("GymV2", await get_title(page));
-    assert.strictEqual(525, await exercise_count(page));
-    await page.screenshot({path: `testing/images/home.png`});
-    await browser.close();
-});
+    const id = "equipment"
+    let option = "t-bar"
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/${option}.png`});
+    assert.strictEqual(1, count);
 
-test("equipment", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
+    option = "kettlebell"
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/${"kettlebell"}.png`});
+    assert.strictEqual(4, count);
+  });
 
-  const id = "equipment"
-  let option = "t-bar"
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/${option}.png`});
-  assert.strictEqual(1, count);
+  await test("search", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    let count
 
-  option = "kettlebell"
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/${"kettlebell"}.png`});
-  assert.strictEqual(4, count);
+    const id = "searchInput"
+    let option = "push"
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/${option}.png`});
+    assert.strictEqual(27, count);
+  });
 
-  await browser.close();
-});
+  await test("target muscle", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    let count
+    
+    const id = "TargetMuscleFilter"
+    let option = "Biceps"
+    
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/${option}.png`});
+    assert.strictEqual(15, count);
+  });
 
-test("search", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
+  await test("synergist muscle", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    let count
+    
+    const id = "SynergistMuscleFilter"
+    let option = "Deltoid"
+    
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/${option}.png`});
+    assert.strictEqual(197, count);
+  });
 
-  const id = "searchInput"
-  let option = "push"
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/${option}.png`});
-  assert.strictEqual(27, count);
+  await test("force", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    let count
+    
+    const id = "ForceSelection"
+    let option = " Pull"// blame db for the space
+    
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/_${option}.png`});
+    assert.strictEqual(276, count);
 
-  await browser.close();
-});
+    option = " Push"// blame db for the space
+    count = await page.evaluate(filter, id, option)
+    sleep(SLEEP_MS)
+    await page.screenshot({path: `testing/images/_${option}.png`});
+    assert.strictEqual(242, count);
+  });
 
-test("target muscle", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
-  
-  const id = "TargetMuscleFilter"
-  let option = "Biceps"
-  
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/${option}.png`});
-  assert.strictEqual(15, count);
+  await test("combined filter", { only: true }, async () => {
+    let page = await get_home_page(browser)
+    let count
+    
+    const force_id = "ForceSelection"
+    let force_option = " Pull"// blame db for the space
+    count = await page.evaluate(filter, force_id, force_option)
+    sleep(SLEEP_MS)
+    assert.strictEqual(276, count);
 
-  await browser.close();
-});
+    const synergist_id = "SynergistMuscleFilter"
+    let synergist_option = "Deltoid"
+    count = await page.evaluate(filter, synergist_id, synergist_option)
+    sleep(SLEEP_MS)
+    assert.strictEqual(82, count);
 
-test("synergist muscle", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
-  
-  const id = "SynergistMuscleFilter"
-  let option = "Deltoid"
-  
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/${option}.png`});
-  assert.strictEqual(197, count);
+    const target_id = "TargetMuscleFilter"
+    let target_option = "Lateral"
+    count = await page.evaluate(filter, target_id, target_option)
+    sleep(SLEEP_MS)
+    assert.strictEqual(14, count);
 
-  await browser.close();
-});
+    const search_id = "searchInput"
+    let search_option = ""
+    count = await page.evaluate(filter, search_id, search_option)
+    sleep(SLEEP_MS)
+    assert.strictEqual(14, count);
 
-test("force", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
-  
-  const id = "ForceSelection"
-  let option = " Pull"// blame db for the space
-  
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/_${option}.png`});
-  assert.strictEqual(276, count);
+    const equipment_id = "equipment"
+    let equipment_option = "dumbbells"
+    count = await page.evaluate(filter, equipment_id, equipment_option)
+    sleep(SLEEP_MS)
+    assert.strictEqual(8, count);
 
-  option = " Push"// blame db for the space
-  count = await page.evaluate(filter, id, option)
-  sleep(SLEEP_MS)
-  await page.screenshot({path: `testing/images/_${option}.png`});
-  assert.strictEqual(242, count);
+    await page.screenshot({path: `testing/images/${"combined_filter_test"}.png`});
+  });
 
-  await browser.close();
-});
-
-test("combined filter", { only: true }, async () => {
-  let browser = await get_browser()
-  let page = await get_home_page(browser)
-  let count
-  
-  const force_id = "ForceSelection"
-  let force_option = " Pull"// blame db for the space
-  count = await page.evaluate(filter, force_id, force_option)
-  sleep(SLEEP_MS)
-  assert.strictEqual(276, count);
-
-  const synergist_id = "SynergistMuscleFilter"
-  let synergist_option = "Deltoid"
-  count = await page.evaluate(filter, synergist_id, synergist_option)
-  sleep(SLEEP_MS)
-  assert.strictEqual(82, count);
-
-  const target_id = "TargetMuscleFilter"
-  let target_option = "Lateral"
-  count = await page.evaluate(filter, target_id, target_option)
-  sleep(SLEEP_MS)
-  assert.strictEqual(14, count);
-
-  const search_id = "searchInput"
-  let search_option = ""
-  count = await page.evaluate(filter, search_id, search_option)
-  sleep(SLEEP_MS)
-  assert.strictEqual(14, count);
-
-  const equipment_id = "equipment"
-  let equipment_option = "dumbbells"
-  count = await page.evaluate(filter, equipment_id, equipment_option)
-  sleep(SLEEP_MS)
-  assert.strictEqual(8, count);
-
-  await page.screenshot({path: `testing/images/${"combined_filter_test"}.png`});
-  await browser.close();
-});
+  browser.close();
+}
+main()
